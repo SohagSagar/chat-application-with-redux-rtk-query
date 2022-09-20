c229cd978fc3aa41b7c288cbf13263cd8b48f080
SohagSagar <sagorsohag29@gmail.com>
2022-09-21 02:12:23 +0600

    message section done

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ import Register from "./pages/Register";
 function App() {
     const isAuthChecked = useAuthCheck();
 
-
-
-
     return !isAuthChecked ? (<div>Cheaking authentication...</div>) : (
         <Router>
             <Routes>
--- src/components/inbox/chatbody/ChatHead.js
+++ src/components/inbox/chatbody/ChatHead.js
@@ -1,12 +1,19 @@
-export default function ChatHead({ avatar, name }) {
+import { useSelector } from "react-redux";
+import gravatar from 'gravatar-url'
+
+export default function ChatHead({ message }) {
+    const {auth} = useSelector(state=>state.auth)
+    const {sender,receiver}=message || {};
+    const receiverName= sender?.email === auth?.email ? receiver.name : sender.name;
+    const receiverEmail= sender?.email === auth?.email ? receiver.email : sender.email;
     return (
         <div className="relative flex items-center p-3 border-b border-gray-300">
             <img
                 className="object-cover w-10 h-10 rounded-full"
-                src={avatar}
-                alt={name}
+                src={gravatar(receiverEmail)}
+                alt={receiverName}
             />
-            <span className="block ml-2 font-bold text-gray-600">{name}</span>
+            <span className="block ml-2 font-bold text-gray-600">{receiverName}</span>
         </div>
     );
 }
